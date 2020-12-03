de50e3d547ed478add573a592b46782628742c44
Nate M <64175378+nmccoard@users.noreply.github.com>
2020-12-03 00:25:19 -0500

    added a functions to validate and email form data

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const submitBtn = document.querySelector("#submitBtn");
+const formName = document.querySelector("#inputName");
+const formActivity = document.querySelector("#chooseActivity");
+const formNum = document.querySelector("#numPeople");
+const formComment = document.querySelector("#comment");
+
 function changePage(e, classToken) {
    document.querySelector(".home").classList.add("hidden");
    document.querySelector(".howTo").classList.add("hidden");
@@ -13,5 +19,47 @@ function changePage(e, classToken) {
    }
    
    e.currentTarget.classList.add("active");
-   //test
 }
+
+function sendEmail(name, emailBody) {
+   Email.send({
+         Host: "smtp.mail.com",
+         Username: "KirtlandFamilyHistory@mail.com",
+         Password: "z#HW2a-_.HAm.vS",
+         To: 'KirtlandFamilyHistory@mail.com',
+         From: "KirtlandFamilyHistory@mail.com",
+         Subject: `Add a brick for ${name}`,
+         Body: emailBody,
+      })
+      .then(function (message) {
+         alert("Message sent successfully")
+      });
+}
+
+submitBtn.addEventListener('click', (e) => {
+   e.preventDefault();
+
+   let sendForm = false;
+   
+   if (formActivity.value === "other" && formComment.value == "") {
+      formComment.style.backgroundColor = "rgba(184, 37, 37, 0.77)";
+      sendForm = false;
+   } else {
+      formComment.style.backgroundColor = "white";
+      if (formName.value != "" && formActivity.value != "" && formNum.value != "") {
+         sendForm = true;
+      } else {
+         sendForm = false;
+      }
+   }
+
+   if(sendForm){
+      let message = `name: ${formName.value} activity: ${formActivity.value} number: ${formNum.value} comment: ${formComment.value}`;
+
+      sendEmail(formName.value, message);
+      formName.value = "";
+      formActivity.value = "";
+      formNum.value = "";
+      formComment.value = "";
+   }
+});
